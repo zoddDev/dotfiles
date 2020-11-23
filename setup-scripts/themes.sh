a28728852c759e2db12428e5fd9c33917087a26b
zodd18 <josiesala@gmail.com>
2020-11-23 23:01:56 +0100

    Minor changes

--- setup-scripts/themes.sh
+++ setup-scripts/themes.sh
@@ -3,16 +3,16 @@
 SETUP_ROOT="$(dirname "$PWD")"
 
 notify-send -i $SETUP_ROOT/dotfiles/setup-scripts/resources/white-brush.png "[INFO]: Copying themes, icons, fonts and scripts..." &
-cp -r $SETUP_ROOT/dotfiles/.scripts $HOME
+cp -rv $SETUP_ROOT/dotfiles/.scripts $HOME
 
 sudo cat $SETUP_ROOT/dotfiles/environment >> /etc/environment
 
 [ -d $HOME/Pictures/Wallpapers ] || mkdir -p $HOME/Pictures/Wallpapers
-cp -r $SETUP_ROOT/dotfiles/Wallpapers $HOME/Pictures/Wallpapers
-cp -r $SETUP_ROOT/dotfiles/.scripts $HOME
-cp -r $SETUP_ROOT/dotfiles/.fonts $HOME
-cp -r $SETUP_ROOT/dotfiles/.icons $HOME
-cp -r $SETUP_ROOT/dotfiles/.themes $HOME
+cp -rv $SETUP_ROOT/dotfiles/Wallpapers $HOME/Pictures/Wallpapers
+cp -rv $SETUP_ROOT/dotfiles/.scripts $HOME
+cp -rv $SETUP_ROOT/dotfiles/.fonts $HOME
+cp -rv $SETUP_ROOT/dotfiles/.icons $HOME
+cp -rv $SETUP_ROOT/dotfiles/.themes $HOME
 
-cp -r $SETUP_ROOT/dotfiles/global-config/* $HOME
+cp -rv $SETUP_ROOT/dotfiles/global-config/. $HOME
 
