8159afa0d42a8f999d08f36c33e4d82e2d0cfe90
zodd18 <josiesala@gmail.com>
2020-11-24 20:35:59 +0100

    Minor changes

--- setup.sh
+++ setup.sh
@@ -5,12 +5,13 @@ theme=$2
 
 shopt -s nocasematch
 case "$option" in
-    "all" ) ./setup-scripts/pacman-packages.sh && ./setup-scripts/aur-packages.sh && ./setup-scripts/themes.sh && ./setup-scripts/rices.sh $theme ;; # $theme is theme name
-    "pacman" ) ./setup-scripts/pacman-packages.sh ;; 
-    "aur" ) ./setup-scripts/aur-packages.sh ;; 
-    "themes" ) ./setup-scripts/themes.sh ;; # Installs themes, icons, fonts, wallpapers and scripts
-    "rice" ) ./setup-scripts/rices.sh $theme ;; # $theme is theme name
+    "all" )     ./setup-scripts/pacman-packages.sh && ./setup-scripts/aur-packages.sh && ./setup-scripts/dm.sh && ./setup-scripts/themes.sh && ./setup-scripts/rices.sh $theme ;; # $theme is theme name
+    "pacman" )  ./setup-scripts/pacman-packages.sh ;; 
+    "aur" )     ./setup-scripts/aur-packages.sh ;; 
+    "dm" )      ./setup-scripts/dm.sh ;; 
+    "themes" )  ./setup-scripts/themes.sh ;; # Installs themes, icons, fonts, wallpapers and scripts
+    "rice" )    ./setup-scripts/rices.sh $theme ;; # $theme is theme name
 
-    *) echo "[ERROR]: no config with name \"$option\" found" && exit 1 ;;
+    *) echo "[ERROR]: no install flag with name \"$option\" found" && exit 1 ;;
 esac
 
