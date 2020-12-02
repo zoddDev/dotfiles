99803ac5c7241c925d20b85e709af90538e786dc
zodd18 <josiesala@gmail.com>
2020-12-02 21:42:54 +0100

    Ayu theme is now a possible rice extension

--- themes/Ayu/.config/neofetch/launch-neofetch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-/bin/neofetch --ascii $HOME/.config/neofetch/ascii --ascii_colors 1 --colors 5 7 7 6 6 7
-#/bin/neofetch --backend kitty --source ~/.config/neofetch/image.png --size '270px'
-
-#/bin/neofetch --backend kitty --source $WALLPAPERS/ayu-theme/572943-Mark_Kirkpatrick-illustration-mountains-Sun-minimalism-low_poly.jpg --size '270px'
