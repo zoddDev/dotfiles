41803747afceecb066405ea7daaccc94ca6a94d7
zodd18 <josiesala@gmail.com>
2020-12-02 21:02:59 +0100

    PinkNordAlternative theme is now a possible rice extension

--- themes/PinkNordAlternative/.config/neofetch/launch-neofetch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-/bin/neofetch --ascii $HOME/.config/neofetch/ascii --ascii_colors 5 --colors 6 7 7 6 6 7
-#/bin/neofetch --backend kitty --source ~/.config/neofetch/image.png --size '270px'
--- themes/PinkNordAlternative/README.md
+++
@@ -1,2 +0,0 @@
-## PinkNord Alternative [(Fleon based)](https://github.com/owl4ce/dotfiles)
-![alt_text](screenshot.png)
