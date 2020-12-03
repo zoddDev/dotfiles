d3ee50545966ee849a815af5c0326ede90cd1d0c
zodd18 <josiesala@gmail.com>
2020-12-03 14:08:07 +0100

    Minor changes

--- setup-scripts/rices.sh
+++ setup-scripts/rices.sh
@@ -12,7 +12,7 @@ function setup_config {
     # args
     config_name=$1
 
-    [ -z $(ls ./themes/$config_name) ] && ./setup-scripts/download-rice.sh $config_name
+    [ -z $(ls -a ./themes/$config_name | grep -v -w '^\.') ] && ./setup-scripts/download-rice.sh $config_name
     
     # backup of .xinitrc, .bashrc and .zshrc
     cp $HOME/.xinitrc $HOME/.xinitrc-backup
