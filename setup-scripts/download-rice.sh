6925e18f48d756d9668595d03cfe6298b6c97ec2
zodd18 <josiesala@gmail.com>
2020-12-03 12:51:58 +0100

    Minor changes

--- setup-scripts/download-rice.sh
+++ setup-scripts/download-rice.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 theme_name=$1
-[ ! -z $theme_name ] && cd $DOTFILES/themes/$theme_name && git submodule update --init .
+[ ! -z $theme_name ] && cd ./themes/$theme_name && git submodule update --init .
