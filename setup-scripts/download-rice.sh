07fa486fd1e1152b97f250df332989ed67869961
zodd18 <josiesala@gmail.com>
2020-12-03 13:41:03 +0100

    Minor changes

--- setup-scripts/download-rice.sh
+++ setup-scripts/download-rice.sh
@@ -3,8 +3,6 @@
 theme_name=$1
 notify-send -i $SETUP_ROOT/dotfiles/setup-scripts/resources/white-brush.png "[INFO]: Downloading... \"$theme_name\" rice..." &
 success=0
-[ ! -z $theme_name ] && cd ./themes/$theme_name && git submodule update --init . && success=1
+[ ! -z $theme_name ] && cd ./themes/$theme_name && git submodule update --init .
 
-[ success -eq 1 ] && notify-send -i $SETUP_ROOT/dotfiles/setup-scripts/resources/white-brush.png "[INFO]: Download for \"$theme_name\" finished successfully!" &
-[ success -eq 1 ] && exit 0
-
+notify-send -i $SETUP_ROOT/dotfiles/setup-scripts/resources/white-brush.png "[INFO]: Download for \"$theme_name\" finished" &
