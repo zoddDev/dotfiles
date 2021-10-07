f62866ca8e805aef90767fcd1bb482643915f778
zodd18 <josiesala@gmail.com>
2021-10-07 13:42:31 +0200

    Minor changes

--- shared-config/.config/polybar/scripts/rofi-poweroff-theme.sh
+++ shared-config/.config/polybar/scripts/rofi-poweroff-theme.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo -e "Logout\nReboot\nShutdown" | rofi -font "Iosevka Nerd Font Mono 9" -show drun -show-icons -width 20 -lines 3 -dmenu -i -config "~/.config/rofi/config.rasi"
+echo -e "Logout\nReboot\nShutdown" | rofi -font "Iosevka Bold 11" -show drun -show-icons -width 20 -lines 3 -dmenu -i -config "~/.config/rofi/config.rasi"
