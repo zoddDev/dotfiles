5646b29ac7dea833fdfb2751bbd95d5f9af74c61
zodd18 <josiesala@gmail.com>
2021-10-07 12:52:35 +0200

    nomacs is floating

--- shared-config/.config/polybar/scripts/theme-swap-theme.sh
+++ shared-config/.config/polybar/scripts/theme-swap-theme.sh
@@ -2,5 +2,5 @@
 
 NUMBER_OF_THEMES=9
 AVAILABLE_THEMES=$(cat $HOME/.config/polybar/scripts/available-themes)
-echo -e $AVAILABLE_THEMES | rofi -font "Iosevka Bold 11" -show drun -show-icons -width 20 -lines $NUMBER_OF_THEMES -dmenu -i -config "~/.config/rofi/config.rasi"
+echo -e $AVAILABLE_THEMES | rofi -font "Iosevka Nerd Font Mono 9" -show drun -show-icons -width 20 -lines $NUMBER_OF_THEMES -dmenu -i -config "~/.config/rofi/config.rasi"
 
