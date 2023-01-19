0043e0b320f572816f04826d58e9e8f09f40f674
zoddDev <josiesala@gmail.com>
2023-01-19 22:38:34 +0100

    Added needed fonts, polybar updates module support for debian based distributions

--- shared-config/.config/polybar/scripts/pacman-updates.sh
+++ shared-config/.config/polybar/scripts/pacman-updates.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-yay -Qu | wc -l
+[[ $(cat /etc/os-release | grep arch) ]] && yay -Qu | wc -l
+#[[ $(cat /etc/os-release | grep debian) ]] && apt list --upgradeable | wc -l
+
+[[ $(cat /etc/os-release | grep debian) ]] && apt list --upgradable 2>/dev/null | wc -l
